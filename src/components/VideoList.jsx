04cf88d5e2af2eb646a7b0eae869335a0b5a3e30
akwong372 <akwong372@gmail.com>
2018-11-11 20:02:44 -0800

    Working on VideoList

--- src/components/VideoList.jsx
+++ src/components/VideoList.jsx
@@ -1,10 +1,11 @@
-var VideoList = () => (
+var VideoList = (props) => (
   <div className="video-list">
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
-    <div><h5><em>videoListEntry</em> view goes here</h5></div>
+      {
+      props.videos.map(item =>
+        <VideoListEntries item = {item}/>
+      )
+    }
+
   </div>
 );
 
@@ -17,3 +18,7 @@ VideoList.propTypes = {
 // In the ES6 spec, files are "modules" and do not share a top-level scope.
 // `var` declarations will only exist globally where explicitly defined.
 window.VideoList = VideoList;
+
+var VideoListEntries = () =>(
+  <div><h5><em>{props.snippet.title}</em> </h5></div>
+)
