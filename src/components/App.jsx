39e9ee35a39e4e026c4851dcf8c006c924dbf795
akwong372 <akwong372@gmail.com>
2018-11-12 19:33:35 -0800

    Complete connecting app to the YouTube API

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -17,27 +17,24 @@ class App extends React.Component {
 
   render() {
     return (
-        <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <VideoPlayer video={this.state.currentVid} />
+          </div>
+          <div className="col-md-5">
+            <VideoList videos={this.state.vidList} onVidClick={this.onVidClick.bind(this)} />
+          </div>
+        </div>
       </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <VideoPlayer video = {this.state.currentVid}/> {/* ignore this for now, i was trying to move to video player */}
-      </div>
-      <div className="col-md-5">
-        <VideoList videos = {this.state.vidList} onVidClick = {this.onVidClick.bind(this)}/> {/* grabs the videos from exampleVideoData */}
-
-      {/* onClick={(this.onVideoTitleClick.bind(this))} */}
-
-      </div>
-    </div>
-  </div>
-    )
+    );
   }
-};
+}
 
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
