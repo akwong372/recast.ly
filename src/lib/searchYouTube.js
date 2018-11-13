3960d8c0aa1c44475f90f2733d28de1d47a628bb
akwong372 <akwong372@gmail.com>
2018-11-12 21:16:35 -0800

    Working on searchYouTube

--- src/lib/searchYouTube.js
+++ src/lib/searchYouTube.js
@@ -1,5 +1,21 @@
-var searchYouTube = (options, callback) => {
+var searchYouTube = (options = {key: window.YOUTUBE_API_KEY, max: 5, query: ''}, callback) => {
   // TODO
+  $.ajax({
+    url: "https://www.googleapis.com/youtube/v3/search",
+    method: 'GET',
+    data: {
+      key: options.key,
+      q: options.query,
+      maxResults: options.max,
+      part: 'snippet',
+      videoEmbeddable: 'true',
+      type: 'video'
+    },
+    contentType: "application/json",
+  }).done( function (data) {
+    console.log(data);
+  } );
+
 };
 
 window.searchYouTube = searchYouTube;
